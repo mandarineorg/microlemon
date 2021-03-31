fc368b514fcf57df91018685d435985d029671e6
andreespirela <andreespirela@outlook.com>
2021-03-31 19:27:38 -0400

    Modifying readme for version 2.0.0

--- mod.ts
+++ mod.ts
@@ -3,3 +3,6 @@ export * from "./src/clients/redis/mod.ts";
 export * from "./src/clients/nats/mod.ts";
 export { Transporters } from "./src/interfaces/connection.ts";
 export type { ConnectionOptions,ConnectionData } from "./src/interfaces/connection.ts";
+export { Microlemon } from "./src/client.ts";
+export { MicrolemonInternal } from "./src/microlemonInternal.ts";
+export type { Client } from "./src/interfaces/client.ts";
