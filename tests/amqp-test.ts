87d77e3892559863d64b5e16de7896d1e61646a6
andreespirela <andreespirela@outlook.com>
2021-10-24 10:56:40 -0400

    Pipe process

--- tests/amqp-test.ts
+++ tests/amqp-test.ts
@@ -5,13 +5,17 @@ Deno.test({
     fn: async () => {
         
         const runReceiver = Deno.run({
-            cmd: ["deno", "run", "--allow-all", "--unstable", "tests/ampq-receiver.ts"]
+            cmd: ["deno", "run", "--allow-all", "--unstable", "tests/ampq-receiver.ts"],
+            stdout: "piped",
+            stderr: "piped"
         });
 
         Deno.sleepSync(2500);
 
         const runSender = Deno.run({
-            cmd: ["deno", "run", "--allow-all", "--unstable", "tests/ampq-sender.ts"]
+            cmd: ["deno", "run", "--allow-all", "--unstable", "tests/ampq-sender.ts"],
+            stdout: "piped",
+            stderr: "piped"
         });
 
         Deno.sleepSync(2500);
